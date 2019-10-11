515d1ae081a0fef9fd5cda6e840d3a89534b6677
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2019-10-11 14:12:51 +0100

    Added DepMap Achilles Gene Effect

---
+++ depmap-achilles-gene-effect/src/main/java/org/intermine/bio/dataconversion/DepmapAchillesGeneEffectConverter.java
@@ -0,0 +1,157 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2018 FlyMine
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.FormattedTextParser;
+import org.intermine.xml.full.Item;
+
+
+/**
+ * 
+ * @author
+ */
+public class DepmapAchillesGeneEffectConverter extends BioDirectoryConverter
+{
+    //
+    private static final String DATASET_TITLE = "DepMap Achilles Gene Effect";
+    private static final String DATA_SOURCE_NAME = "DepMap Public 19Q3";
+
+    private static final String TAXON_ID = "9606"; // Human Taxon ID
+
+    private static final String CN_CSV_FILE = "Achilles_gene_effect.csv";
+
+    private Map<String, String> genes = new HashMap<String, String>();
+    private Map<String, String> cellLines = new HashMap<String, String>();
+
+    private String organismIdentifier; // Not the taxon ID. It references the object that is created into the database.
+
+    /**
+     * Constructor
+     * @param writer the ItemWriter used to handle the resultant items
+     * @param model the Model
+     */
+    public DepmapAchillesGeneEffectConverter(ItemWriter writer, Model model) {
+        super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
+    }
+
+    /**
+     * 
+     *
+     * {@inheritDoc}
+     */
+    public void process(File dataDir) throws Exception {
+
+        Map<String, File> files = readFilesInDir(dataDir);
+
+        organismIdentifier = getOrganism(TAXON_ID);
+
+        processEffect(new FileReader(files.get(CN_CSV_FILE)));
+
+    }
+
+    private Map<String, File> readFilesInDir(File dir) {
+        Map<String, File> files = new HashMap<String, File>();
+        for (File file : dir.listFiles()) {
+            files.put(file.getName(), file);
+        }
+        return files;
+    }
+
+    private void processEffect(Reader reader) throws ObjectStoreException, IOException {
+        Iterator<?> lineIter = FormattedTextParser.parseCsvDelimitedReader(reader);
+        // header
+        String[] firstLine = (String[]) lineIter.next();
+        ArrayList<String> genes = new ArrayList<String>();
+        for(int i = 1; i < firstLine.length; i++) {
+            String formattedGene = firstLine[i].split(" ")[0].trim();
+            genes.add(formattedGene);
+        }
+
+        //lineIter.next();
+        while (lineIter.hasNext()) {
+            String[] line = (String[]) lineIter.next();
+
+            String cellLine = line[0];
+            for(int i = 1; i < line.length; i++) {
+                String cnvValue = line[i];
+                String theGeneForThisItem = genes.get(i-1);
+                Item CopyNumberItem;
+
+                CopyNumberItem = createItem("DepMapCopyNumber");
+
+                if(!cellLine.isEmpty()) {
+                    CopyNumberItem.setReference("depMapID", getCellLine(cellLine));
+                } else {
+                    continue;
+                }
+
+                if(!theGeneForThisItem.isEmpty()) {
+                    CopyNumberItem.setReference("gene", getGene(theGeneForThisItem));
+                } else {
+                    continue;
+                }
+
+                if(!cnvValue.isEmpty()) {
+                    CopyNumberItem.setAttribute("value", cnvValue);
+                } else {
+                    continue;
+                }
+
+                store(CopyNumberItem);
+                //cellLines.put(cellLine, CopyNumberItem.getIdentifier());
+            }
+        }
+    }
+
+    public String getGene(String identifier) {
+        String refId = genes.get(identifier);
+        if (refId == null) {
+            Item gene = createItem("Gene");
+            gene.setAttribute("symbol", identifier);
+            try {
+                store(gene);
+            } catch (ObjectStoreException e) {
+                throw new RuntimeException("failed to store gene with primary identifier: " + identifier, e);
+            }
+            refId = gene.getIdentifier();
+            genes.put(identifier, refId);
+        }
+        return refId;
+    }
+
+    public String getCellLine(String identifier) {
+        String refId = cellLines.get(identifier);
+        if (refId == null) {
+            Item cl = createItem("CellLine");
+            cl.setAttribute("DepMapID", identifier);
+            try {
+                store(cl);
+            } catch (ObjectStoreException e) {
+                throw new RuntimeException("failed to store cell line with DepMapID: " + identifier, e);
+            }
+            refId = cl.getIdentifier();
+            cellLines.put(identifier, refId);
+        }
+        return refId;
+    }
+}
